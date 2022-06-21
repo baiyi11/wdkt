3b8c388b668571b74bf02a18edae0156a5b6f424
baiyi <sat119@qq.com>
2022-06-21 11:15:54 +0800

    bug修正

--- wdkt/__init__.py
+++ wdkt/__init__.py
@@ -3,13 +3,13 @@ from wdkt.settings import config
 from wdkt.blueprints import qa_bp,user_bp
 from wdkt.exts  import db,mail,migrate,moment,cors
 
-def create_app(config_name=None):
+def create_app(config_name :str=None):
     """工厂函数"""
     if config_name is None:
         config_name = "development"
-
     else:
         config_name=config_name
+
     app=Flask("wdkt")
     app.config.from_object(config[config_name])
 
